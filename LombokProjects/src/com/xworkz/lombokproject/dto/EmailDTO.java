49f8d7d8812d70182e7e35a01184357c6792d68a
Your Name <you@example.com>
2024-10-16 10:50:14 +0530

    lombok files

---
+++ LombokProjects/src/com/xworkz/lombokproject/dto/EmailDTO.java
@@ -0,0 +1,24 @@
+package com.xworkz.lombokproject.dto;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+public class EmailDTO implements Serializable {
+
+	private String from;
+	private String to;
+	private String subject;
+	private String message;
+
+}
